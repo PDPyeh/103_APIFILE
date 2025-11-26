69a298c437304ae99d3f7448bd88e4e6b5a11d81
Pradipa Y <pdpyyo@gmail.com>
2025-11-26 10:36:13 +0700

    config db index models

---
+++ models/komik.js
@@ -0,0 +1,30 @@
+module.exports = (sequelize, DataTypes) => {
+  const Komik = sequelize.define('Komik', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true, 
+      autoIncrement: true,
+      allowNull: false
+    },
+    judul: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    penulis: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    deskripsi: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+      imageType: DataTypes.STRING,
+      imageName: DataTypes.STRING,
+      imageData: DataTypes.BLOB('long')
+  },
+  
+  {
+    tableName: 'Komik',
+  });
+    return Komik;
+};
